fd75d0520a871b651925b417310a1d8321e63dcb
Philipp Wolfer <ph.wolfer@gmail.com>
2016-03-15 08:53:06 +0100

    Use MusicBrainz fav icon in oneboxes

--- plugin.rb
+++ plugin.rb
@@ -7,4 +7,7 @@ require_relative 'engine/musicbrainz_artist_onebox'
 require_relative 'engine/musicbrainz_release_onebox'
 require_relative 'engine/musicbrainz_releasegroup_onebox'
 
+register_asset "stylesheets/musicbrainz.scss"
+register_asset "favicons/musicbrainz.png"
+
 Onebox.options.load_paths.push(File.join(File.dirname(__FILE__), "templates"))
