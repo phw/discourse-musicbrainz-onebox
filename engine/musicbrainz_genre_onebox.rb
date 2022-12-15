267f304db051273354790c8ad6530e84bfe7d195
Philipp Wolfer <ph.wolfer@gmail.com>
2022-12-15 22:50:44 +0100

    Basic genre onebox (#19)

---
+++ engine/musicbrainz_genre_onebox.rb
@@ -0,0 +1,39 @@
+require_relative "musicbrainz"
+
+module Onebox
+  module Engine
+    class MusicBrainzGenreOnebox
+      include Engine
+      include LayoutSupport
+      include MusicBrainz
+
+      matches_entity("genre")
+      always_https
+
+      private
+
+      def url
+        "https://#{match[:domain]}/ws/2/#{@@entity}/#{match[:mbid]}?fmt=json&inc=url-rels"
+      end
+
+      def match
+        @match ||= @url.match(@@matcher)
+      end
+
+      def data
+        return @data if @data
+
+        @data = {
+          link: @url,
+          id: raw["id"],
+          title: raw["name"].capitalize,
+        }
+
+        disambiguation
+        wikidata
+
+        return @data
+      end
+    end
+  end
+end
